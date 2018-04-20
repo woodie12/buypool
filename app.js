2915b849da810525b4095d091e5309edd8168408
Connie Zhou <connie@10-151-211-8.vlan912.client.wireless.uic.edu>
2018-04-19 22:54:49 -0500

    first page css1

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+
+const users = require('./routes/users');
+const requests = require('./routes/requests');
+const connection = require('./connection');
+
+app.use(express.static(__dirname + '/src'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/users/api', users);
+app.use('/requests/api',requests);
+http.createServer(app).listen(3000);
+console.log("listen on port 3000");
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config.js');
+
+// router.get('/', function(req, res, next) {
+//   // GET/users/ route
+//   res.send({name:config.admin.name});
+// });
+
+// app/routes.js
+module.exports = function(app, passport) {
+    // app.get('/', function(req, res) {
+    //     res.status(200).send({
+    //         message: 'OK',
+    //         data: []
+    //     })
+    // });
+    //
+    // app.get('/login', function(req, res) {
+    //
+    // });
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }),
+        function(req, res) {
+            console.log("hello");
+
+            if (req.body.remember) {
+                req.session.cookie.maxAge = 1000 * 60 * 3;
+            } else {
+                req.session.cookie.expires = false;
+            }
+            res.redirect('/');
+        });
+
+
+    // show the signup form
+    // app.get('/signup', function(req, res) {
+    //     // render the page and pass in any flash data if it exists
+    //     res.render('signup.ejs', { message: req.flash('signupMessage') });
+    // });
+
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+
+    // app.get('/profile', isLoggedIn, function(req, res) {
+    //     res.render('profile.ejs', {
+    //         user : req.user // get the user out of session and pass to template
+    //     });
+    // });
+
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
+module.exports = router;
