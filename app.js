6702caa8b50f6f69a6e9e401775b7583b02966eb
Yu Zhang <yzhan189@illinois.edu>
2018-04-23 01:26:42 -0500

    router /* bug fixed

--- app.js
+++ app.js
@@ -25,22 +25,20 @@ app.use(cookieParser());
 app.use(cookieSession({ secret: 'my secret', cookie: { maxAge : 1200000 } }));
 
 app.use(session({
-    secret: 'vidyapathaisalwaysrunning',
-    resave: true,
-    saveUninitialized: true
+  secret: 'vidyapathaisalwaysrunning',
+  resave: true,
+  saveUninitialized: true
 } ));
 var allowCrossDomain = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  next();
 };
 app.use(allowCrossDomain);
 
 
-app.route('/*').get(function(req, res) {
-    return res.sendFile(path.join(__dirname, './src/index.html'));
-});
+
 
 
 app.use(bodyParser.json());
@@ -53,6 +51,16 @@ app.use(passport.session());
 app.use('/users/api', require('./routes/users'));
 
 app.use('/requests/api',requests);
-http.createServer(app).listen(3000);
-console.log("listen on port 3000");
+
+// this should the last router
+app.route('/*').get(function(req, res) {
+  return res.sendFile(path.join(__dirname, './src/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
+
 module.exports = app;
