cfdecd5c91be52b8ff557f35290f983644945930
Yu Zhang <yzhan189@illinois.edu>
2018-04-22 23:47:25 -0500

    user signup issue
    most updated version

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,90 @@
 const express = require('express');
-
-const app = express();
+const router = express.Router();
+const config = require('../config.js');
+const connection = require('../connection');
+const transporter = require('../email');
 const passport = require('passport');
 
 
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true, // allow flash messages
+    session: false
+  }),
+  function(req, res) {
+    console.log("hello");
 
-const router = express.Router();
-const connection = require('../connection');
+    if (req.body.remember) {
+      req.session.cookie.maxAge = 1000 * 60 * 3;
+    } else {
+      req.session.cookie.expires = false;
+    }
+
+    res.status(200).json({ user: req.user, message: "Welcome!"
+    });
+  });
+
+
+router.post('/signup', function(req, res, next) {
+  passport.authenticate('local-signup', {
+    successRedirect : '/', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  },function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.redirect('/login'); }
+    console.log("USER API: signup user with further info");
+    console.log(user);
+
+    connection.acquire(function(err, con) {
+      let update_query = ' UPDATE User SET ';
+      let isFirst = true;
+
+      if (req.body.username){
+        if (!isFirst) {update_query += ' , '};
+        update_query += ' username = "'+req.body.username+'" ';
+        isFirst = false;
+      };
+      if (req.body.phone){
+        if (!isFirst) {update_query += ' , '};
+        update_query += ' phone = '+req.body.phone;
+        isFirst = false;
+      };
+      if (req.body.address){
+        if (!isFirst) {update_query += ' , '};
+        update_query += ' address = "'+ req.body.address+'" ';
+        isFirst = false;
+      };
+
+      update_query += ' WHERE email = "'+ req.body.email+'" ';
+
+      console.log("USER API: "+update_query);
+
+      con.query(update_query,
+        function(err, result) {
+          con.release();
+          if (err) {
+            res.send(err);
+          } else {
+            res.send({
+              status: 200,
+              message: 'Update User successfully'
+            });
+          }
+        });
+    });
+
+  })(req, res, next);
+});
+
+
+
 
 // put rating to a user
 // example put users/api/ratings/DDTT_45
 router.put('/ratings/:id', function(req,res){
-  console.log("put rating of a user");
+  console.log("USER API: put rating of a user");
   connection.acquire(function(err, con){
     // get number of people having rated this user
     con.query('SELECT ratingWeight,rating FROM User WHERE userId = ?', req.params.id,
@@ -23,7 +96,7 @@ router.put('/ratings/:id', function(req,res){
           if (result.length === 0){
             res.send("userId not exist");
           }else{
-            console.log(result[0].rating);
+
             const newWeight = result[0].ratingWeight + 1;
             const aveRating = (result[0].ratingWeight * result[0].rating + parseInt(req.body.rating))/newWeight;
             con.query('UPDATE User SET rating = ?, ratingWeight = ? WHERE userId = ?',
@@ -42,72 +115,10 @@ router.put('/ratings/:id', function(req,res){
   });
 });
 
-
-
-// app/routes.js
-// module.exports = function(router, passport) {
-    // app.get('/', function(req, res) {
-    //     res.status(200).send({
-    //         message: 'OK',
-    //         data: []
-    //     })
-    // });
-    //
-    // app.get('/login', function(req, res) {
-    //
-    // });
-
-    // process the login form
-    // yzhan189: add "session: false" fix serialize err
-    router.post('/login', passport.authenticate('local-login', {
-            // successRedirect : '/', // redirect to the secure profile section
-            // failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true, // allow flash messages
-            session: false
-        }),
-        function( req, res) {
-        console.log(req.user);
-            if(!req.user){
-                res.status(500).json({ message: 'wrong password ot no account'})
-            }else {
-
-                // if (req.body.remember) {
-                //     req.session.cookie.maxAge = 1000 * 60 * 3;
-                // } else {
-                //     req.session.cookie.expires = false;
-                // }
-
-                res.status(200).json({
-                    user: req.user, message: "Welcome!"
-                });
-            }
-        });
-
-    // process the signup form
-    router.post('/signup', passport.authenticate('local-signup', {
-        // successRedirect : '/', // redirect to the secure profile section
-        // failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }), function(req, res) {
-        console.log(req.user);
-            if(!req.user){
-                res.status(500).json({ message: 'already exist'})
-            }else {
-                console.log("hello signup");
-
-                res.status(200).json({user: req.user, message: "ok!"});
-            }
-        });
-
-
-
-
-
-
 // get all information of a user
 // example get users/api/DDTT_45
 router.get('/:id', function(req,res){
-    console.log("get info of user");
+    console.log("USER API: get info of user");
     connection.acquire(function(err, con){
         con.query('SELECT * FROM User WHERE userId = ?', req.params.id,
             function(err, result) {
@@ -124,20 +135,7 @@ router.get('/:id', function(req,res){
 // update a user information
 // example put users/api/DDTT_45
 router.put('/:id', function(req,res){
-  console.log("put a user");
-
-  connection.acquire(function(err, con){
-    con.query('SELECT * FROM User WHERE userId = ?', req.params.id,
-      function(err, result) {
-        con.release();
-        if (err){
-          res.send(err);
-        }else{
-          res.send(result);
-        }
-      });
-  });
-
+  console.log("USER API: put a user");
 
   connection.acquire(function(err, con) {
     let update_query = ' UPDATE User SET ';
@@ -159,8 +157,9 @@ router.put('/:id', function(req,res){
       isFirst = false;
     };
 
-
     update_query += ' WHERE userId = ? ';
+
+    console.log("USER API: "+update_query);
 
     con.query(update_query, req.params.id,
       function(err, result) {
@@ -176,7 +175,6 @@ router.put('/:id', function(req,res){
       });
   });
 });
-
 
 
 // invitation: recommend user to user
@@ -204,14 +202,6 @@ router.get('/recommendation/:requestId', function(req, res) {
   });
 });
 
-    router.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
-// };
-
-// route middleware to make sure
-// function isLoggedIn(req, res, next) {
 // invite, send email to invite
 router.post('/invite', function(req, res) {
   const requestId = req.body.requestId;
@@ -273,82 +263,6 @@ router.post('/invite', function(req, res) {
       });
   });
 })
-
-
-
-// router.get('/', function(req, res, next) {
-//   // GET/users/ route
-//   res.send({name:config.admin.name});
-// });
-
-// // app/routes.js
-// module.exports = function(app, passport) {
-//     // app.get('/', function(req, res) {
-//     //     res.status(200).send({
-//     //         message: 'OK',
-//     //         data: []
-//     //     })
-//     // });
-//     //
-//     // app.get('/login', function(req, res) {
-//     //
-//     // });
-//
-//     // process the login form
-//     app.post('/login', passport.authenticate('local-login', {
-//             successRedirect : '/', // redirect to the secure profile section
-//             failureRedirect : '/login', // redirect back to the signup page if there is an error
-//             failureFlash : true // allow flash messages
-//         }),
-//         function(req, res) {
-//             console.log("hello");
-//
-//             if (req.body.remember) {
-//                 req.session.cookie.maxAge = 1000 * 60 * 3;
-//             } else {
-//                 req.session.cookie.expires = false;
-//             }
-//             res.redirect('/');
-//         });
-//
-//
-//     // show the signup form
-//     // app.get('/signup', function(req, res) {
-//     //     // render the page and pass in any flash data if it exists
-//     //     res.render('signup.ejs', { message: req.flash('signupMessage') });
-//     // });
-//
-//     // process the signup form
-//     app.post('/signup', passport.authenticate('local-signup', {
-//         successRedirect : '/', // redirect to the secure profile section
-//         failureRedirect : '/signup', // redirect back to the signup page if there is an error
-//         failureFlash : true // allow flash messages
-//     }));
-//
-//
-//     // app.get('/profile', isLoggedIn, function(req, res) {
-//     //     res.render('profile.ejs', {
-//     //         user : req.user // get the user out of session and pass to template
-//     //     });
-//     // });
-//
-//
-//     app.get('/logout', function(req, res) {
-//         req.logout();
-//         res.redirect('/');
-//     });
-// };
-//
-// // route middleware to make sure
-// function isLoggedIn(req, res, next) {
-//
-//     // if user is authenticated in the session, carry on
-//     if (req.isAuthenticated())
-//         return next();
-//
-//     // if they aren't redirect them to the home page
-//     res.redirect('/');
-// }
 
 
 module.exports = router;
